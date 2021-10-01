8a0e1540e0deb37dd1840f84fbb699574d3d3320
Jesus Esteban Díez <56318729+Jesusestebandiez@users.noreply.github.com>
2021-10-01 21:11:01 +0200

    Add files via upload

---
+++ upgrade-basic-algorithms/iteration_3_var1.3.js
@@ -0,0 +1,19 @@
+// ejer OPera 1.1 Multiplica 10 por 5 y muestra el resultado mediante console.log
+it('La variable multi es la multiplicación de num10 por num5',() =>{
+expect(multi).to.equal(50);
+});
+console.log();
+
+//ejer OPera 1.2 Divide 10 por 2 y muestra el resultado en un console.log.
+it('La variable divi es la division de num10 por num2',() =>{
+expect(divi).to.equal(5);
+});
+console.log();
+//ejer OPer 1.3 Muestra mediante un console.log el resto de dividir 15 por 9.
+it('La variable resto es el resulfinal de num15 por num9',() =>{
+    expect(divi).to.equal(1,6666666666666666666666666666667);
+    });
+    console.log();
+//ejer OPer 1.4 Usa el correcto operador de asignación que resultará en x = 15, teniendo dos variables y = 10 y z = 5.
+
+//ejer OPer 1.5 Usa el correcto operador de asignación que resultará en x = 50, teniendo dos variables y = 10 y z = 5.

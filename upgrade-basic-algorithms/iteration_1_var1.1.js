c37cdab13b258c975fa6079dcc274e52f603b680
Jesus Esteban Díez <56318729+Jesusestebandiez@users.noreply.github.com>
2021-10-01 19:52:54 +0200

    Add files via upload

---
+++ upgrade-basic-algorithms/iteration_1_var1.1.js
@@ -0,0 +1,17 @@
+//Variables_simples
+
+//ejer 1.1
+let myFavoriteHero = "Hulk";
+console.log(myFavoriteHero);
+//ejer 1.2
+let x = '50';
+console.log(x);
+
+//ejer 1.3
+let h = '5';
+let y = '10';
+console.log(); 
+
+//ejer 1.4
+let z = '5 + 10'; 
+console.log(z);
---
+++ upgrade-basic-algorithms/iteration_2_var1.2.js
@@ -0,0 +1,22 @@
+ //Var_avan ejer 1.1 Dado el siguiente javascript, cambia el valor de la propiedad age a 25.
+const character = {
+name: 'Jack Sparrow', age: 10 
+};
+console.log();
+// Var_avan ejer 1.2 Declara 3 variables con los nombres y valores siguientes
+ let firstName = 'Jon';
+ let lastName = 'Snow';
+        age = '24';
+'Soy Jon Snow, tengo 24 años y me gustan los lobos.'
+console.log();
+
+// Var_avan ejer 1.3 Dado el siguiente javascript, imprime con un console.log la suma del precio de ambos juguetes
+const toy1 ={ name: 'Buss myYear', price: 19};
+const toy2 ={ name: 'Buss myYear', price: 29};
+console.log();
+
+//Var_avan ejer 1.4 Dado el siguiente javascript, actualiza el valor de la variable globalBasePrice a 25000 y actualiza la propiedad finalPrice de todos los coches con el valor de su propiedad basePrice más el valor de la variable globalBasePrice
+let globalBasePrice = 10000;
+const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000 };
+const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
+console.log();
